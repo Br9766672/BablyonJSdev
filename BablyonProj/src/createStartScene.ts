262bceee78383b4acd43b4f030401a402dddbe68
Bryce Rae <supersonicgun666@gmail.com>
2023-10-26 12:21:04 +0100

    Week 8

--- BablyonProj/src/createStartScene.ts
+++ BablyonProj/src/createStartScene.ts
@@ -4,21 +4,49 @@ import {
     Scene,
     ArcRotateCamera,
     Vector3,
+    Vector4,
     HemisphericLight,
     MeshBuilder,
     Mesh,
+    StandardMaterial,
+    Texture,
     Light,
     Camera,
     Engine,
   } from "@babylonjs/core";
   
   
-  function createBox(scene: Scene) {
+  function createBox(scene: Scene , px: number, py: number, pz: number , sx: number , sy: number ,sz: number ) {
     let box = MeshBuilder.CreateBox("box",{size: 1}, scene);
-    box.position.y = 3;
+    box.position = new Vector3(px, py, pz)
+    box.scaling = new Vector3(sx, sy, sz)
     return box;
   }
+  const mat = new StandardMaterial("mat");
+  const texture = new Texture("https://assets.babylonjs.com/environments/numbers.jpg");
+  mat.diffuseTexture = texture;
 
+  var columns = 6;
+  var rows = 1;
+
+  const faceUV = new Array(6);
+
+  for (let i = 0; i < 6; i++) {
+      faceUV[i] = new Vector4(i / columns, 0, (i + 1) / columns, 1 / rows);
+  }
+
+  const options = {
+      faceUV: faceUV,
+      wrap: true
+  };
+  function createFacedBox(scene: Scene , px : number, py : number, pz : number)
+  {
+    let facedBox = MeshBuilder.CreateBox("facedBox",{size: 1}, scene)
+    facedBox.position = new Vector3(px, py, pz)
+    facedBox.material = mat
+    return facedBox;
+  }
+  
   
   function createLight(scene: Scene) {
     const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
@@ -66,6 +94,7 @@ import {
     interface SceneData {
       scene: Scene;
       box?: Mesh;
+      facedBox?: Mesh;
       light?: Light;
       sphere?: Mesh;
       ground?: Mesh;
@@ -75,7 +104,8 @@ import {
     let that: SceneData = { scene: new Scene(engine) };
     //that.scene.debugLayer.show();
 
-    createBox(that.scene);
+    createBox(that.scene , 5 , 5 , 10 , 1 , 1 , 2);
+    createFacedBox(that.scene, 10, 1 ,5)
     createLight(that.scene);
     createSphere(that.scene);
     createGround(that.scene);
