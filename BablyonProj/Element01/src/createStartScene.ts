2f3c2e0b8a2247c71873c97b144210d136e5f6a0
Bryce Rae <supersonicgun666@gmail.com>
2023-11-16 12:30:49 +0000

    Week 11

--- BablyonProj/Element01/src/createStartScene.ts
+++ BablyonProj/Element01/src/createStartScene.ts
@@ -1,59 +1,117 @@
-//import "@babylonjs/core/Debug/debugLayer";
-//import "@babylonjs/inspector";
 import {
     Scene,
     ArcRotateCamera,
     Vector3,
     Vector4,
     HemisphericLight,
+    SpotLight,
     MeshBuilder,
     Mesh,
-    StandardMaterial,
-    Texture,
     Light,
     Camera,
     Engine,
+    StandardMaterial,
+    Texture,
+    Color3,
+    Space,
+    ShadowGenerator,
+    PointLight,
+    DirectionalLight,
   } from "@babylonjs/core";
-  
-  
-  function createBox(scene: Scene , px: number, py: number, pz: number , sx: number , sy: number ,sz: number ) {
+ 
+  function createBox(scene: Scene, px: number, py: number, pz: number, sx: number, sy: number, sz: number, rotation: boolean) {
     let box = MeshBuilder.CreateBox("box",{size: 1}, scene);
-    box.position = new Vector3(px, py, pz)
-    box.scaling = new Vector3(sx, sy, sz)
+    box.position = new Vector3(px, py, pz);
+    box.scaling = new Vector3(sx, sy, sz);
+
+    if (rotation) {
+      scene.registerAfterRender(function () {
+        box.rotate(new Vector3(4, 8, 2)/*axis*/, 0.02/*angle*/, Space.LOCAL);
+      });
+    }
     return box;
   }
-  const mat = new StandardMaterial("mat");
-  const texture = new Texture("https://assets.babylonjs.com/environments/numbers.jpg");
-  mat.diffuseTexture = texture;
 
-  var columns = 6;
-  var rows = 1;
+  
+  function createFacedBox(scene: Scene, px: number, py: number, pz: number) {
+    const mat = new StandardMaterial("mat");
+    const texture = new Texture("https://assets.babylonjs.com/environments/numbers.jpg");
+    mat.diffuseTexture = texture;
 
-  const faceUV = new Array(6);
+    var columns = 6;
+    var rows = 1;
 
-  for (let i = 0; i < 6; i++) {
-      faceUV[i] = new Vector4(i / columns, 0, (i + 1) / columns, 1 / rows);
+    const faceUV = new Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        faceUV[i] = new Vector4(i / columns, 0, (i + 1) / columns, 1 / rows);
+    }
+
+    const options = {
+        faceUV: faceUV,
+        wrap: true
+    };
+
+    let box = MeshBuilder.CreateBox("tiledBox", options, scene);
+    box.material = mat;
+    box.position = new Vector3(px, py, pz);
+
+    scene.registerAfterRender(function () {
+        box.rotate(new Vector3(2, 6, 4)/*axis*/, 0.02/*angle*/, Space.LOCAL);
+    });
+    return box;
   }
 
-  const options = {
-      faceUV: faceUV,
-      wrap: true
-  };
-  function createFacedBox(scene: Scene , px : number, py : number, pz : number)
-  {
-    let facedBox = MeshBuilder.CreateBox("facedBox",{size: 1}, scene)
-    facedBox.position = new Vector3(px, py, pz)
-    facedBox.material = mat
-    return facedBox;
+  function createTorus(scene: Scene, px: number, py: number, pz: number) {
+    const torus = MeshBuilder.CreateTorus("torus", {});
+    torus.position = new Vector3(px, py, pz);
+    scene.registerAfterRender(function () {
+      torus.rotate(new Vector3(4, 8, 2)/*axis*/, 0.02/*angle*/, Space.LOCAL);
+    });
+    return torus;
   }
-  
-  
-  function createLight(scene: Scene) {
+
+  function createPolyhedra(scene: Scene, t: number, s: number, px: number, py: number, pz: number) {
+    const polyhedra = MeshBuilder.CreatePolyhedron("shape", {type: t, size: s}, scene);
+    polyhedra.position = new Vector3(px, py, pz);
+    scene.registerAfterRender(function () {
+      polyhedra.rotate(new Vector3(4, 8, 2)/*axis*/, 0.02/*angle*/, Space.LOCAL);
+    });
+    return polyhedra;
+  }
+
+  function createAnyLight(scene: Scene, index: number, px: number, py: number, pz: number, colX: number, colY: number, colZ: number, mesh: Mesh) {
+    switch (index) {
+      case 1: 
+        const hemiLight = new HemisphericLight("hemiLight", new Vector3(px, py, pz), scene);
+        hemiLight.intensity = 0.1;
+        return hemiLight;
+        break;
+      case 2: 
+        const spotLight = new SpotLight("spotLight", new Vector3(px, py, pz), new Vector3(0, -1, 0), Math.PI / 3, 10, scene);
+        spotLight.diffuse = new Color3(colX, colY, colZ); //0.39, 0.44, 0.91
+        let shadowGenerator = new ShadowGenerator(1024, spotLight);
+        shadowGenerator.addShadowCaster(mesh);
+        shadowGenerator.useExponentialShadowMap = true;
+        return spotLight;
+        break;
+      case 3: 
+        const pointLight = new PointLight("pointLight", new Vector3(px, py, pz), scene);
+        pointLight.diffuse = new Color3(colX, colY, colZ); //0.39, 0.44, 0.91
+        shadowGenerator = new ShadowGenerator(1024, pointLight);
+        shadowGenerator.addShadowCaster(mesh);
+        shadowGenerator.useExponentialShadowMap = true;
+        return pointLight;
+        break;
+    }
+  }
+ 
+  function createHemiLight(scene: Scene) {
     const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
+    light.intensity = 0.3;
     return light;
   }
-  
+
   function createSphere(scene: Scene) {
     let sphere = MeshBuilder.CreateSphere(
       "sphere",
@@ -64,12 +122,14 @@ import {
     return sphere;
   }
   
-  function createGround(scene: Scene) {
+  function createGround(scene: Scene, w: number, h: number, px: number) {
     let ground = MeshBuilder.CreateGround(
       "ground",
-      { width: 6, height: 6 },
+      { width: w, height: h },
       scene,
     );
+    ground.receiveShadows = true;
+    ground.position.x = px;
     return ground;
   }
   
@@ -89,26 +149,54 @@ import {
     camera.attachControl(true);
     return camera;
   }
-  
+ 
   export default function createStartScene(engine: Engine) {
     interface SceneData {
       scene: Scene;
       box?: Mesh;
-      facedBox?: Mesh;
+      faceBox?: Mesh;
       light?: Light;
+      hemisphericLight?: HemisphericLight;
       sphere?: Mesh;
+      torus?: Mesh;
+      polyhedra?: Mesh;
       ground?: Mesh;
       camera?: Camera;
     }
   
     let that: SceneData = { scene: new Scene(engine) };
-    //that.scene.debugLayer.show();
+    that.scene.debugLayer.show();
+  
+   
+    that.hemisphericLight = createHemiLight(that.scene);
 
-    createBox(that.scene , 5 , 5 , 10 , 1 , 1 , 2);
-    createFacedBox(that.scene, 10, 1 ,5)
-    createLight(that.scene);
-    createSphere(that.scene);
-    createGround(that.scene);
-    createArcRotateCamera(that.scene);
+    that.faceBox = createFacedBox(that.scene, -10, 2, 0);
+    that.light = createAnyLight(that.scene, 2, -10, 5, 0, 0.75, 0.12, 0.91, that.faceBox);
+
+    
+    that.box = createBox(that.scene, -5, 2, 0, 3, 2, 1, true);
+
+    
+    that.torus = createTorus(that.scene, 0, 2, 0);
+    that.light = createAnyLight(that.scene, 2, 0, 5, 0, 0.12, 0.64, 0.86, that.torus);
+
+    
+    that.polyhedra = createPolyhedra(that.scene, 1, 1, 5, 2, 0);
+
+    
+    that.polyhedra = createPolyhedra(that.scene, 12, 1, 10, 2, 0);
+    that.light = createAnyLight(that.scene, 2, 10, 5, 0, 0.24, 0.24, 0.91, that.polyhedra);
+
+    
+    that.box = createBox(that.scene, -13, 2.5, 0, 0.5, 5, 9, false); //wall 
+    that.box = createBox(that.scene, -8, 2.5, 0, 0.5, 5, 9, false); //wall
+    that.box = createBox(that.scene, -3, 2.5, 0, 0.5, 5, 9, false); //wall 
+    that.box = createBox(that.scene, 2, 2.5, 0, 0.5, 5, 9, false); //wall 
+    that.box = createBox(that.scene, 7, 2.5, 0, 0.5, 5, 9, false); //wall 
+    that.box = createBox(that.scene, 12, 2.5, 0, 0.5, 5, 9, false); //wall 
+
+    
+    that.ground = createGround(that.scene, 25, 10, -0.5);
+    that.camera = createArcRotateCamera(that.scene);
     return that;
   }
--- Dev.md
+++ Dev.md
@@ -1,2 +1,3 @@
 # coding notes
 Setting up
+reminder to copy HavokPhysics.wasm into the debs vite nodemodules file
